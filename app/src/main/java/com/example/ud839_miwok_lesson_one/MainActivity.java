1d3304f0672fec4ed9be4dff62901a3d8f54d75c
Ankit Singh <ankitsinghniti@gmail.com>
2022-02-02 22:23:57 +0530

    MiWok APP

--- app/src/main/java/com/example/ud839_miwok_lesson_one/MainActivity.java
+++ app/src/main/java/com/example/ud839_miwok_lesson_one/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ud839_miwok_lesson_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView numbers = (TextView)findViewById(R.id.numbers);
+        numbers.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+                TextView family =(TextView)findViewById(R.id.family);
+                family.setOnClickListener(new OnClickListener() {
+                                              @Override
+                                              public void onClick(View view) {
+                                                  Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                                                  startActivity(familyIntent);
+                                              }
+                                          });
+                        TextView colors = (TextView)findViewById(R.id.colors);
+                        colors.setOnClickListener(new OnClickListener() {
+                                                      @Override
+                                                      public void onClick(View view) {
+                                                          Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                                                          startActivity(colorsIntent);
+                                                      }
+                                                  });
+                                TextView phrases = (TextView)findViewById(R.id.phrases);
+                                phrases.setOnClickListener(new OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                                        startActivity(phrasesIntent);
+                                    }
+                                });
     }
 }
--- app/src/main/java/com/example/ud839_miwok_lesson_one/NumbersActivity.java
+++ app/src/main/java/com/example/ud839_miwok_lesson_one/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.ud839_miwok_lesson_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create array of words
+        ArrayList<String> words = new ArrayList<>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        for(int i = 0; i < words.size(); i++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+
     }
 }
